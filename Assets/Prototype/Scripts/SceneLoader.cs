cb1f7c2fe9dff5c67e631ea1b6daaabf4b9ae3a3
Andy <81387021+snailythefan@users.noreply.github.com>
2021-07-01 11:04:49 -0500

    build
    we made a build and this has all the changes for it

---
+++ Assets/Prototype/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadAndy()
+    {
+    	SceneManager.LoadScene("Andy");
+    }
+
+    public void LoadMegan()
+    {
+    	SceneManager.LoadScene("Megan");
+    }
+
+    public void LoadMenu()
+    {
+    	SceneManager.LoadScene("MainMenu");
+    }
+}
