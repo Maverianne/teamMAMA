3156e9dacb65dc097f98e70fc8b159a2cf8dd380
Mariana <64marianamv@gmail.com>
2021-08-23 13:33:01 -0500

    Walk step
    Have to match the step

---
+++ Assets/StepSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepSound : MonoBehaviour
+{
+    private AudioSource step;
+
+
+    private void Start()
+    {
+        step = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void StepPlay()
+    {
+        step.Play();
+    }
+}
