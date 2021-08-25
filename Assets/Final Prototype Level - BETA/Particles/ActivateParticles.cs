83902cd8d42b1bd05e3752da96d3b35920ef51b5
Megan Matthews <sketchbook.megan@gmail.com>
2021-08-25 13:27:01 +0100

    Megan - Leaf particle prefab created and added to scene. Works with 'space' but needs collider info in the "ActivateParticles" script

---
+++ Assets/Final Prototype Level - BETA/Particles/ActivateParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateParticles : MonoBehaviour
+{
+    [SerializeField] ParticleSystem LeafParticles = null;
+    [SerializeField] ParticleSystem LeafParticles2 = null;
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayParticle();
+        }
+    }
+
+    public void PlayParticle()
+    {
+        LeafParticles.Play();
+        LeafParticles2.Play();
+    }
+}
